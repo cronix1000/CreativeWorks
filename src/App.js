d53ce3c502f15fe38f12c3a7a2df2f15164f5d56
Khushwinder Singh <khushwindersinghdhillon79@gmail.com>
2023-01-02 14:12:26 -0500

    deleted unnecesary files and setup the required project scripts

--- src/App.js
+++ src/App.js
@@ -1,44 +1,7 @@
-import './App.css';
-import '.'
+import "./App.css";
+import ".";
 //import from the api folder
 
+function App() {}
 
-var Simplex = require('perlin-simplex')
-var simplex = new Simplex()
-
-function CreateCharacter() {
-    async function OnRequestRaces() {
-        //call the GenerateRaces function from the GenerateRaces.js
-        const response = fetch('https://www.dnd5eapi.co/api/races')
-            .then(res => res.json())
-            .catch(err => { throw err });
-        return response;
-    }
-
-    var raceSelection = document.getElementById("race");
-    var options = OnRequestRaces();
-
-    var races;
-
-    options.then(races)
-    console.log(races)
-
-
-    //for (var i = 0; i < options.length; i++) {
-    //    var opt = options[4][i].name;
-    //    var el = document.createElement("option");
-    //    el.textContent = opt;
-    //    el.value = opt;
-    //    raceSelection.appendChild(el);
-    //}
-  return (
-      <div className="CreateCharacter">
-          <select id="race">
-              <option>Choose a race</option>
-          </select>
-       
-    </div>
-  );
-}
-
-export default CreateCharacter;
+export default App;
